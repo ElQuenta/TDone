4f90943844517ba03ad74df1aa11b301e487a01a
tarnishedAlexander <alex4750384@gmail.com>
2023-10-17 01:14:02 +0000

    first notes desing

---
+++ app/src/main/java/com/example/tdone/notes.kt
@@ -0,0 +1,11 @@
+package com.example.tdone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class notes: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.notes_layout)
+    }
+}
