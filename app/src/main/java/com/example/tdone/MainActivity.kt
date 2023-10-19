449532d3192976bb6f16a1fe6fdc06d2da328df8
Jorge K <kallavallejosjorgeluis@gmail.com>
2023-10-19 08:58:55 -0400

    a

--- app/src/main/java/com/example/tdone/MainActivity.kt
+++ app/src/main/java/com/example/tdone/MainActivity.kt
@@ -10,33 +10,11 @@ import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        val IMAGE_CODE = 100
-    }
 
-    private lateinit var imageView: ImageView
-    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageView = findViewById(R.id.imgGalery)
-        button = findViewById(R.id.btnCamera)
-
-        button.setOnClickListener() {
-            pickImage()
-        }
-    }
-    fun pickImage() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_CODE)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == IMAGE_CODE && resultCode == RESULT_OK){
-            imageView.setImageURI(data?.data)
-        }
-    }
 }
