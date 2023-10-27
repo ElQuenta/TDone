173c07ef6b4ebad2335a3480eb80e7e91cd556f2
Jorge K <kallavallejosjorgeluis@gmail.com>
2023-10-26 22:17:29 -0400

    imagen_circular

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     //DataStore Persistencia de Datos
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
