e39262e04f8cac69c47d16667af9aa6ca2038cb2
Miguel <angeloquenta@gmail.com>
2023-10-30 19:29:29 -0400

    Validación por correo

--- app/src/main/java/com/example/tdone/auth/SignIn.kt
+++ app/src/main/java/com/example/tdone/auth/SignIn.kt
@@ -2,6 +2,7 @@ package com.example.tdone.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tdone.MainActivity
@@ -34,8 +35,7 @@ class SignIn : AppCompatActivity() {
 
                 firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
                     if (it.isSuccessful) {
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
+                        userVerified(true)
                     } else {
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
 
@@ -52,9 +52,36 @@ class SignIn : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        if(firebaseAuth.currentUser != null){
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        if (firebaseAuth.currentUser != null) {
+            userVerified(true)
         }
     }
+
+    fun userVerified(firstTime: Boolean) {
+        val user = firebaseAuth.currentUser
+        user!!.reload().addOnCompleteListener { reloadTask ->
+            if (reloadTask.isSuccessful) {
+                if (user.isEmailVerified) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val message = "not verify yet"
+                    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                    if (firstTime) {
+                        user.sendEmailVerification()
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    val message2 = "You have 15 seconds to verify your e-mail"
+                                    Toast.makeText(this, message2, Toast.LENGTH_SHORT).show()
+                                    Handler().postDelayed({
+                                        userVerified(false)
+                                    }, 15000)
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+
 }
