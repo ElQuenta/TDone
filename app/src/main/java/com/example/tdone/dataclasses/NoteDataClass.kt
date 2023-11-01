0604d8d7d8c39a0f4647d86a9faa7c1d45eb4367
Miguel <angeloquenta@gmail.com>
2023-10-31 23:03:08 -0400

    fecha de actualización

--- app/src/main/java/com/example/tdone/dataclasses/NoteDataClass.kt
+++ app/src/main/java/com/example/tdone/dataclasses/NoteDataClass.kt
@@ -1,6 +1,9 @@
 package com.example.tdone.dataclasses
 
+import android.icu.text.SimpleDateFormat
 import java.io.Serializable
+import java.util.Calendar
+import java.util.Locale
 
 data class NoteDataClass(
     val noteTittle: String,
@@ -9,5 +12,9 @@ data class NoteDataClass(
     var hasVinculation: Boolean = false,
     var noteTags: List<TagDataClass> = listOf(),
     var noteFront: ImageDataClass? = null, //investigar referencia al archivo de las imagenes
-    var noteBackground: Int
+    var noteBackground: Int,
+    var lastUpdate: Long = SimpleDateFormat(
+        "ddMMyyyy",
+        Locale.getDefault()
+    ).format(Calendar.getInstance().timeInMillis).toLong()
 ): Serializable
